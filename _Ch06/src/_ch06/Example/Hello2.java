9e3b6c613ac6b915763f4d16cd6f9d965a09ea64
BANTaeHee <ban7598@gmail.com>
2023-01-25 22:30:58 +0900

    2023-01-25 Standard_of_Java 실습

---
+++ _Ch06/src/_ch06/Example/Hello2.java
@@ -0,0 +1,10 @@
+ package _ch06.Example;
+// 현재 소스파일의 이름은 Hello2.java
+// 하나의 소스파일에는 하나의 클래스만 작성하는 것이 바람직하다
+public class Hello2 {} // 기본적으로 소스파일의 이름은 public class 이름과 일치해야한다.
+	   class Hello3 {} // 클래스가 여러개인 경우 소스파일명은 public class 이름과 일치해야한다.
+	   class Hello4 {}
+	    
+//public class Hello3 {} // 하나의 소스파일에는 하나의 public class만 허용한다.
+	   
+//public class Hello3 {} -> 소스파일의 이름과 public class 이름과 불일치이므로 오류
