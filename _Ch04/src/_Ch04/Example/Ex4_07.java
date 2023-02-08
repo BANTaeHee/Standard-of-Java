f1dbe30cba93a63ffe3e9c9db3b0e1ca3a731254
BANTaeHee <ban7598@gmail.com>
2023-02-08 09:40:17 +0900

    2023-02-07 Standard_of_Java 실습 예제

--- _Ch04/src/_Ch04/Example/Ex4_07.java
+++ _Ch04/src/_Ch04/Example/Ex4_07.java
@@ -5,7 +5,7 @@ public class Ex4_07 {
 	public static void main(String[] args) {
 
 		int num = 0;
-		
+		 
 		for(int i = 1; i <= 5; i++) {
 			num = (int) (Math.random() * 6) + 1;
 			System.out.println(num);
