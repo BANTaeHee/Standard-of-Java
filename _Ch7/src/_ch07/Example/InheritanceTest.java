688ecce6577b78166e58a1b294da114c6caa7f48
BANTaeHee <ban7598@gmail.com>
2023-03-11 00:28:01 +0900

    2023-03-11 Standard_of_Java 포함 관계 실습

---
+++ _Ch7/src/_ch07/Example/InheritanceTest.java
@@ -0,0 +1,35 @@
+package _ch07.Example;
+
+class MyPoint {
+	int x;
+	int y;
+}
+
+//class Circle extends MyPoint {		// 상속
+//	int r;
+//}
+
+class Circle {		// 포함관계
+	MyPoint p = new MyPoint();
+	int r;
+}
+
+public class InheritanceTest {
+
+	public static void main(String[] args) {
+		Circle c = new Circle();
+//		c.x = 1;			// 상속
+//		c.y = 2;
+//		c.r = 3;
+//		System.out.println("c.x=" + c.x);
+//		System.out.println("c.y=" + c.y);
+//		System.out.println("c.r=" + c.r);
+		c.p.x = 1;
+		c.p.y = 2;
+		c.r = 3;
+		System.out.println("c.p.x=" + c.p.x);
+		System.out.println("c.p.y=" + c.p.y);
+		System.out.println("c.r=" + c.r);
+	}
+
+}
